4cc6844786a52c32c71ccae12f89c8bc6509aa30
MatSchwarz <matias.schwarz314@gmail.com>
2024-09-11 17:11:09 -0300

    Update Semi-smoothNewtonMethod.py

--- Método de Newton/Semi-smoothNewtonMethod.py
+++ Método de Newton/Semi-smoothNewtonMethod.py
@@ -5,16 +5,19 @@ import scipy.stats
 from scipy import linalg
 
 
-def Newton_Semismooth(Q,q,x0,tol):
+def Newton_Semismooth(matriz_Q,vetor_q,x0,tol):
     matriz_T = T(Q)
     vetor_b = b(matriz_T,q).T
-    x_depois =  np.array((np.linalg.inv(Matriz_P(x0) + matriz_T)@vetor_b).T)[0]
-    criterio_parar = np.linalg.norm(np.diag(Matriz_P(x_depois)) - (np.diag(Matriz_P(x0))))
+    x_depois = np.linalg.inv(Matriz_P(x0) + matriz_T)@vetor_b
+    criterio_parar = np.linalg.norm(np.diag(Matriz_P(x_depois)) - 
+                                    (np.diag(Matriz_P(x0))))
     x_antes = x_depois
 
     while criterio_parar > tol:
-        x_depois =  np.array((np.linalg.inv(Matriz_P(x_antes) + matriz_T)@vetor_b).T)[0]
+        x_depois =  np.linalg.inv(Matriz_P(x_antes) + matriz_T)@vetor_b
+        criterio_parar = np.linalg.norm(np.diag(Matriz_P(x_depois)) - 
+                                        (np.diag(Matriz_P(x_antes))))
         x_antes = x_depois
-        criterio_parar = np.linalg.norm(np.diag(Matriz_P(x_depois)) - (np.diag(Matriz_P(x_antes))))
 
     return x_depois
+
