cec4534dc5c1dca792663ae3136d2655fbdde9f4
MatSchwarz <matias.schwarz314@gmail.com>
2024-09-12 15:02:14 -0300

    Update SemismoothNewtonMethod.py

--- Método de Newton/SemismoothNewtonMethod.py
+++ Método de Newton/SemismoothNewtonMethod.py
@@ -6,8 +6,8 @@ from scipy import linalg
 
 
 def Newton_Semismooth(matriz_Q,vetor_q,x0,tol):
-    matriz_T = T(Q)
-    vetor_b = b(matriz_T,q).T
+    matriz_T = T(matriz_Q)
+    vetor_b = b(matriz_T,vetor_q).T
     x_depois = np.linalg.inv(Matriz_P(x0) + matriz_T)@vetor_b
     criterio_parar = np.linalg.norm(np.diag(Matriz_P(x_depois)) - 
                                     (np.diag(Matriz_P(x0))))
