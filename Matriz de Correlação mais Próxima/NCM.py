ce24d64bbc2d2290eb968190ecd53a0649f88d11
MatSchwarz <matias.schwarz314@gmail.com>
2024-09-13 20:45:19 -0300

    Update NCM.py

--- Matriz de Correlação mais Próxima/NCM.py
+++ Matriz de Correlação mais Próxima/NCM.py
@@ -10,3 +10,19 @@ def Df(X,G):
     Df = np.linalg.inv(Diag(diag(V(X))))@(e(X) - diag(V(X)@G_chapeu(G)))
     return Df
 
+def SM_ncm(X_input,G_input,iter,tol):
+    i = 1
+    X_inicial = X_input
+    X_proximo = Diag(Df(X_inicial,G_input)) + G_chapeu(G_input)
+
+    while i < iter:
+        i = i+1 
+        print("iteracao",i-1)
+        print(V(X_proximo) @ X_proximo)
+        X_inicial = X_proximo
+        X_proximo = Diag(Df(X_inicial,G_input)) + G_chapeu(G_input)
+
+        if np.linalg.norm(np.diag(V(X_proximo) @ X_proximo) - e(X)) < tol:
+            break
+    
+    return X_proximo, iter
