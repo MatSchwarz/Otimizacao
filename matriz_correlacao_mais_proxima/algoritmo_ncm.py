f6aee3925981e81e8a092643b6a3090043c3b9e7
MatSchwarz <matias.schwarz314@gmail.com>
2024-09-14 13:36:30 -0300

    mudanças nos testes de ncm

--- matriz_correlacao_mais_proxima/algoritmo_ncm.py
+++ matriz_correlacao_mais_proxima/algoritmo_ncm.py
@@ -4,27 +4,27 @@ import scipy.stats
 from scipy import linalg
 from func_auxiliares_ncm import *
 
-#Função responsável por fazer uma iteração do algoritmo
 
-def Df(X,G):
-    Df = np.linalg.inv(Diag(diag(V(X))))@(e(X) - diag(V(X)@G_chapeu(G)))
-    return Df
+def diferencial_f(x,g):
+    """Função responsável por fazer uma iteração do algoritmo"""
+    df = np.linalg.inv(Diag(diag(V(x))))@(e(x) - diag(V(x)@G_chapeu(g)))
+    return df
 
 
-#Algoritmo de Nearest Correlation Matrix
 def sm_ncm(x_input,g_input,iter,tol):
+    """Algoritmo de Nearest Correlation Matrix"""
     i = 1
     X_inicial = x_input
-    X_proximo = Diag(Df(X_inicial,g_input)) + G_chapeu(g_input)
+    X_proximo = Diag(diferencial_f(X_inicial,g_input)) + G_chapeu(g_input)
 
     while i < iter:
         i = i+1 
         print("iteracao",i-1)
         print(V(X_proximo) @ X_proximo)
         X_inicial = X_proximo
-        X_proximo = Diag(Df(X_inicial,g_input)) + G_chapeu(g_input)
+        X_proximo = Diag(diferencial_f(X_inicial,g_input)) + G_chapeu(g_input)
 
         if np.linalg.norm(np.diag(V(X_proximo) @ X_proximo) - e(X_proximo)) < tol:
             break
     
-    return X_proximo, iter
+    return V(X_proximo) @ X_proximo, i
